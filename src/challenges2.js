f4b946b38a775ca9988db681151ee36aec43b8c8
Filipe Lima <cimofelipe4@gmail.com>
2022-07-01 09:39:31 -0300

    Requisito 11 refatorado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,30 +1,19 @@
 // Desafio 11
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
 function generatePhoneNumber(array) {
-  const message =
-    'não é possível gerar um número de telefone com esses valores';
-  if (array.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
+  const message = 'não é possível gerar um número de telefone com esses valores';
+  if (array.length !== 11) return 'Array com tamanho incorreto.';
 
   for (let index = 0; index < array.length; index += 1) {
     let contador = 0;
-    if (array[index] < 0 || array[index] > 9) {
-      return message;
-    }
+    if (array[index] < 0 || array[index] > 9) return message;
 
     for (let count = 0; count < array.length; count += 1) {
-      if (array[index] === array[count]) {
-        contador += 1;
-      }
-
-      if (contador >= 3) {
-        return message;
-      }
+      if (array[index] === array[count]) contador += 1;
+      if (contador >= 3) return message;
     }
   }
-  // usar slice para refatorar >
-  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+  return `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 11).join('')}`;
 }
 
 console.log(generatePhoneNumber(numbers));
