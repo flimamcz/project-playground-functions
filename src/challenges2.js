89f4d1df98c045b5cb4039154e2a6a91979fb87c
Filipe Lima <cimofelipe4@gmail.com>
2022-07-01 09:29:55 -0300

    Requisito: 11 [x]

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,18 +1,29 @@
 // Desafio 11
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
 function generatePhoneNumber(array) {
+  const message =
+    'não é possível gerar um número de telefone com esses valores';
   if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
 
-  // Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do array se repetir 3 vezes ou mais; [x]
-
   for (let index = 0; index < array.length; index += 1) {
-    if ((array[index] < 0) || (array[index] > 9)) {
-      return 'não é possível gerar um número de telefone com esses valores';
+    let contador = 0;
+    if (array[index] < 0 || array[index] > 9) {
+      return message;
+    }
+
+    for (let count = 0; count < array.length; count += 1) {
+      if (array[index] === array[count]) {
+        contador += 1;
+      }
+
+      if (contador >= 3) {
+        return message;
+      }
     }
   }
-
+  // usar slice para refatorar >
   return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
 }
 
