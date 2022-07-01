1a1eef8ece06ea81df39a8be9350bb84ed11300c
Filipe Lima <cimofelipe4@gmail.com>
2022-06-30 22:40:06 -0300

    Finalizando requisito 13. OBS: Falta o requisito em obs no code

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  // Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do array se repetir 3 vezes ou mais; [x]
+
+  for (let index = 0; index < array.length; index += 1) {
+    if ((array[index] < 0) || (array[index] > 9)) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
 }
+
+console.log(generatePhoneNumber(numbers));
 
 // Desafio 12
 function triangleCheck() {
